57f5df15e91e26ef08d5ffbbc0773d2918119c6f
Islavath Sathish Naik <sathishnaikislavath@gmail.com>
2025-07-09 21:56:50 +0530

    Updated

--- src/services/blogService.ts
+++ src/services/blogService.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import api from './api';
 import { BlogPost } from '../types/blog';
 
-const API_BASE = '/api/blogs';
+const API_BASE = '/blogs'; // api.ts already has /api as base
 
 export const BlogService = {
   async getAll(params?: any): Promise<BlogPost[]> {
-    const res = await axios.get<BlogPost[]>(API_BASE, { params });
+    const res = await api.get<BlogPost[]>(API_BASE, { params });
     return res.data;
   },
   async getBySlug(slug: string): Promise<BlogPost> {
-    const res = await axios.get<BlogPost>(`${API_BASE}/${slug}`);
+    const res = await api.get<BlogPost>(`${API_BASE}/${slug}`);
     return res.data;
   },
   async getByCategory(category: string): Promise<BlogPost[]> {
-    const res = await axios.get<BlogPost[]>(`${API_BASE}/category/${encodeURIComponent(category)}`);
+    const res = await api.get<BlogPost[]>(`${API_BASE}/category/${encodeURIComponent(category)}`);
     return res.data;
   }
 }; 
