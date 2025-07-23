9f88a6ca8aaea5ff70fb82be8d4fe84c08facf21
Sathishnaik786 <sathishnaikislavath@gmail.com>
2025-07-23 14:39:31 +0530

    Updated SEO again

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ const adminRoutes = require('./routes/admin');
 const adminAuth = require('./middleware/adminAuth');
 const newsletterController = require('./controllers/newsletterController');
 const appleCarouselRoutes = require('./routes/appleCarousel');
+const blogRoutes = require('./routes/blog');
 
 const app = express();
 
@@ -128,6 +129,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Routes
 app.use('/api/tools', toolRoutes);
+app.use('/api/blogs', blogRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/tags', tagRoutes);
 app.use('/api/business-functions', businessRoutes);
