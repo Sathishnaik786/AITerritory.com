fca68bb61c7ee54cc263312ecb29b958bc708aae
Sathishnaik786 <sathishnaikislavath@gmail.com>
2025-08-11 01:30:39 +0530

    All are Done

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -583,12 +583,39 @@ const BlogDetail: React.FC = () => {
           transition={{ duration: 0.6, ease: "easeOut" }}
         >
           <div className="max-w-4xl mx-auto px-4 py-6">
-            {/* Breadcrumbs - Client-side only */}
-            {isClient && (
-              <div className="mb-4">
-                <PageBreadcrumbs />
-              </div>
-            )}
+            {/* Breadcrumb Navigation */}
+            <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-4 text-sm text-gray-600 dark:text-gray-400">
+              <nav className="flex" aria-label="Breadcrumb">
+                <ol className="inline-flex items-center space-x-1 md:space-x-3">
+                  <li className="inline-flex items-center">
+                    <a href="/" className="inline-flex items-center text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300">
+                      <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                      </svg>
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <div className="flex items-center">
+                      <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
+                      </svg>
+                      <a href="/blog" className="ml-1 text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 md:ml-2">Blog</a>
+                    </div>
+                  </li>
+                  <li aria-current="page">
+                    <div className="flex items-center">
+                      <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
+                      </svg>
+                      <span className="ml-1 text-gray-500 dark:text-gray-400 md:ml-2">
+                        {blog.title.length > 20 ? `${blog.title.substring(0, 20)}...` : blog.title}
+                      </span>
+                    </div>
+                  </li>
+                </ol>
+              </nav>
+            </div>
 
             {/* Category */}
             <div className="text-xs text-gray-500 uppercase tracking-wider mb-2">
